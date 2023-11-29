765f13eeb77cbe56fc06c0b587a17f94703bcab5
Michael Ogbuma <ogbumamichael@gmail.com>
2023-11-29 12:13:56 +0100

    - render data with use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// In src/App.tsx
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface Todo {
+  _id: string;
+  text: string;
+  completed: boolean;
+}
+
+const baseUrl = "http://localhost:5000/api/v1/";
+const todoUrl = `${baseUrl}todos/`;
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(todoUrl);
+      const { data } = await response.json();
+      setTodos(data);
+    };
+    fetchData();
+  }, []);
+
+  const addTodo = async () => {
+    const response = await fetch(todoUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: newTodo }),
+    });
+    const { data } = await response.json();
+    setTodos([data, ...todos]);
+    setNewTodo("");
+  };
+
+  const toggleTodo = async (id: string, completed: boolean) => {
+    const response = await fetch(`${todoUrl}${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newTodo,
+        //  completed: newCompleted  !!TODOOOOOOOOOOOOOOTODOOOO
+      }),
+    });
+    const { data } = await response.json();
+    const updatedTodos = todos.map((todo) =>
+      todo._id === data._id ? data : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = async (id: string) => {
+    await fetch(`${todoUrl}${id}`, {
+      method: "DELETE"
+    });
+    const updatedTodos = todos.filter((todo) => todo._id !== id);
+      setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MERN Todo App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+              onClick={() => toggleTodo(todo._id, todo.completed)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
